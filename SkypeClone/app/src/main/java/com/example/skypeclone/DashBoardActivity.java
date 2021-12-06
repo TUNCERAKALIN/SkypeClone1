9f7bf152cece0f16ed23d847a19d553527f0dc29
tuncerschmidt <tuncerakalin407@gmail.com>
2021-12-05 23:27:34 -0600

    Jitsi library needed always

--- SkypeClone/app/src/main/java/com/example/skypeclone/DashBoardActivity.java
+++ SkypeClone/app/src/main/java/com/example/skypeclone/DashBoardActivity.java
@@ -26,6 +26,7 @@ public class DashBoardActivity extends AppCompatActivity {
         URI serverUrl;
         //
         //
+        //
 
         try {
             serverUrl = new URI("https://meet.jit.si");
